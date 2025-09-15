da71f299e7d4b7fdb73c9b80f9ec19adc58850d1
Priyankaendluri <endlurupriyanka@gmail.com>
2025-09-15 11:38:43 +0530

    Add files via upload

---
+++ suduko solver.py
@@ -0,0 +1,56 @@
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(board[i][j] if board[i][j] != 0 else ".", end=" ")
+        print()
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check row
+    if any(board[row][x] == num for x in range(9) if x != col):
+        return False
+
+    # Check column
+    if any(board[y][col] == num for y in range(9) if y != row):
+        return False
+
+    # Check 3x3 box
+    box_x = col // 3
+    box_y = row // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True  # Solved
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0  # Backtrack
+
+    return False
+
